7f5a12233cd3ce0a5fe83412703c2917d0e24312
Thiago Kaique <thiago.kaique@stayfilm.com.br>
2022-12-17 17:54:23 -0300

    [feature]

--- src/main.jsx
+++ src/main.jsx
@@ -20,17 +20,19 @@ function describe(text)
   
   for(const phrase of parsed)
   {
+    const phrase_container = <div class="phrase"></div>
+    phrase_container.parent(container)
     for(const word of phrase)
     {
       const ref = {}
       const data = 
       <div class="card">
         <div class="title item">{word.name}</div>
-        <div class="title item">{word.type}</div>
+        <div class="item subtitle">{word.type}</div>
         <div class="body item" ref={ref} id="tags"></div>
       </div>
   
-      data.parent(container)
+      data.parent(phrase_container)
       for(const tag of word.tags)
       {
         const ctag = <div>{tag}</div>
